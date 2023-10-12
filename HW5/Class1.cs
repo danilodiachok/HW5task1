33ef714bf914eab1f75068a27d57fc56f2e216d9
danilo_cj <danilo_cj@DESKTOP-BT8DAVV>
2023-10-12 03:15:04 +0300

    Add project files.

---
+++ HW5/Class1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IDeveloper
+{
+    string Tool { get; set; }
+    void Create();
+    void Destroy();
+}
+
+
+public class Programmer : IDeveloper
+{
+    public string Tool { get; set; }
+    public string Language { get; set; }
+
+    public Programmer(string tool, string language)
+    {
+        Tool = tool;
+        Language = language;
+    }
+
+    public void Create()
+    {
+        Console.WriteLine($"Програміст мовою {Language} з використанням {Tool} створює щось.");
+    }
+
+    public void Destroy()
+    {
+        Console.WriteLine($"Програміст мовою {Language} з використанням {Tool} видляє щось.");
+    }
+}
+public class Builder : IDeveloper
+{
+    public string Tool { get; set; }
+    public string ConstructionType { get; set; }
+
+    public Builder(string tool, string constructionType)
+    {
+        Tool = tool;
+        ConstructionType = constructionType;
+    }
+
+    public void Create()
+    {
+        Console.WriteLine($"Будівельник, що будує {ConstructionType} та використовує {Tool}, будує щось.");
+    }
+
+    public void Destroy()
+    {
+        Console.WriteLine($"Будівельник, що будує {ConstructionType} та використовує {Tool}, демонтує щось.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        List<IDeveloper> developers = new List<IDeveloper>
+        {
+            new Programmer("Visual Studio", "C#"),
+            new Builder("плиткоріз", "Житловий будинок"),
+            new Programmer("vs code", "Java"),
+            new Builder("шуроповерт", "Школа")
+        };
+
+       
+        foreach (var developer in developers)
+        {
+            developer.Create();
+            developer.Destroy();
+        }
+
+
+    }
+}
